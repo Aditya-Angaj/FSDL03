49e4398e8afcfb278ce57b56d0a654a3a3e299f0
Aditya-Angaj <adityaangaj@gmail.com>
2025-04-04 07:27:33 +0000

    new progranm

--- week09/studentController.js
+++ week09/studentController.js
@@ -1,13 +1,21 @@
 app.controller('studentController', function($scope, studentService) {
-    $scope.students = studentService.getStudents();
-  
-    $scope.addStudent = function() {
-      studentService.addStudent($scope.student);
-      $scope.student = {}; // Clear the form
-    };
-  
-    $scope.deleteStudent = function(index) {
-      studentService.deleteStudent(index);
-    };
-  });
-  
+  // Load students from the service
+  $scope.students = studentService.getStudents();
+
+  // Function to add a student
+  $scope.addStudent = function() {
+    if ($scope.student && $scope.student.name && $scope.student.age) {
+      studentService.addStudent($scope.student); // Add student to service
+      $scope.students = studentService.getStudents(); // Refresh displayed list
+      $scope.student = {}; // Clear form
+    } else {
+      alert("Please fill out all fields!");
+    }
+  };
+
+  // Function to delete a student
+  $scope.deleteStudent = function(index) {
+    studentService.deleteStudent(index); // Delete student using the service
+    $scope.students = studentService.getStudents(); // Refresh list after deletion
+  };
+});
--- week09/studentService.js
+++ week09/studentService.js
@@ -1,16 +1,20 @@
 app.service('studentService', function() {
-    var students = [];
-  
-    this.getStudents = function() {
-      return students;
-    };
-  
-    this.addStudent = function(student) {
-      students.push(student);
-    };
-  
-    this.deleteStudent = function(index) {
-      students.splice(index, 1);
-    };
-  });
+  var students = []; // Array to store student data
+
+  // Return the list of students
+  this.getStudents = function() {
+    return students;
+  };
+
+  // Add a student to the array
+  this.addStudent = function(student) {
+    students.push(student);
+  };
+
+  // Remove a student from the array
+  this.deleteStudent = function(index) {
+    students.splice(index, 1);
+  };
+});
+
   
